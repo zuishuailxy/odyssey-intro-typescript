7d2ae1edd0a4c679ca74e459acce1ff708e8f6f3
Liz Hennessy <lizhennessy@Lizs-MacBook-Pro.local>
2024-06-25 14:10:25 -0600

    lesson 9: codegen

--- src/datasources/listing-api.ts
+++ src/datasources/listing-api.ts
@@ -1,9 +1,10 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
+import { Listing } from "../types";
 
 export class ListingAPI extends RESTDataSource {
   baseURL = "https://rt-airlock-services-listing.herokuapp.com/";
 
-  getFeaturedListings() {
-    return this.get<any[]>("featured-listings");
+  getFeaturedListings(): Promise<Listing[]> {
+    return this.get<Listing[]>("featured-listings");
   }
 }
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,4 +1,6 @@
-export const resolvers = {
+import { Resolvers } from "./types";
+
+export const resolvers: Resolvers = {
   Query: {
     featuredListings: (_, __, { dataSources }) => {
       return dataSources.listingAPI.getFeaturedListings();
