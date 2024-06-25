544d3ec9d62e0436006026c87c7feeb325fc59ce
Liz Hennessy <lizhennessy@Lizs-MacBook-Pro.local>
2024-06-25 14:20:41 -0600

    lesson 13: resolver chains

--- src/datasources/listing-api.ts
+++ src/datasources/listing-api.ts
@@ -1,5 +1,5 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
-import { Listing } from "../types";
+import { Listing, Amenity } from "../types";
 
 export class ListingAPI extends RESTDataSource {
   baseURL = "https://rt-airlock-services-listing.herokuapp.com/";
@@ -10,5 +10,10 @@ export class ListingAPI extends RESTDataSource {
 
   getListing(listingId: string): Promise<Listing> {
     return this.get<Listing>(`listings/${listingId}`);
+  }
+
+  getAmenities(listingId: string): Promise<Amenity[]> {
+    console.log("Making a follow-up call for amenities with ", listingId);
+    return this.get<Amenity[]>(`listings/${listingId}/amenities`);
   }
 }
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,4 +1,5 @@
 import { Resolvers } from "./types";
+import { validateFullAmenities } from "./helpers";
 
 export const resolvers: Resolvers = {
   Query: {
@@ -8,5 +9,12 @@ export const resolvers: Resolvers = {
     listing: (_, { id }, { dataSources }) => {
       return dataSources.listingAPI.getListing(id);
     },
+  },
+  Listing: {
+    amenities: ({ id, amenities }, _, { dataSources }) => {
+      return validateFullAmenities(amenities)
+    ? amenities
+    : dataSources.listingAPI.getAmenities(id);
+    }
   }
 };
